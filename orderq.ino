a76ed0ef412095739898c623d19b51494f4d3c33
Dennis de Greef <github@link0.net>
2015-02-17 21:42:03 +0100

    Setup strip

--- orderq.ino
+++ orderq.ino
@@ -1,8 +1,25 @@
+//
+// OrderQ
+//
+// @license MIT 2015 Dennis de Greef <github.com/dennisdegreef/orderq/LICENSE>
+// 
+
 #include <Adafruit_NeoPixel.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define PIN 6
 
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  strip.begin();
+  strip.show();
 }
 
 void loop() {
